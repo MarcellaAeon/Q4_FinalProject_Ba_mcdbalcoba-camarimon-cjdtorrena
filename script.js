924be94acd9dde8e6c6df265b9c1a36959fe5aaa
Martheena Balcoba <143463459+MarcellaAeon@users.noreply.github.com>
2024-05-31 14:42:31 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+function userCredentials(){
+    var name = document.getElementById("name");
+    var user = document.getElementById("user");
+    var email = document.getElementById("email");
+    var pass = document.getElementById("pass");
+    var address = document.getElementById("address");
+    var gender = document.getElementById("gender");
+    var interests = document.getElementById("interests");
+
+    document.getElementById("c-name").innerHTML = "Name: " + name;
+    document.getElementById("c-user").innerHTML = "Username: " + user;
+    document.getElementById("c-email").innerHTML = "Email: " + email;
+    document.getElementById("c-pass").innerHTML = "Password: " + pass;
+    document.getElementById("c-address").innerHTML = "Address: " + address;
+    document.getElementById("c-gender").innerHTML = "Gender: " + gender;
+    document.getElementById("c-interests").innerHTML = "Interests: " + interests;
+}
+function noNameInput(){
+    var bgcolor = document.getElementById("name");
+    var hex = "#A68585";
+    bgcolor.style.border = hex;
+}
+function noUserInput(){
+    var bgcolor = document.getElementById("user");
+    var hex = "#A68585";
+    bgcolor.style.border = hex;
+}
+function noEmailInput(){
+    var bgcolor = document.getElementById("email");
+    var hex = "#A68585";
+    bgcolor.style.border = hex;
+}
+function noPassInput(){
+    var bgcolor = document.getElementById("pass");
+    var hex = "#A68585";
+    bgcolor.style.border = hex;
+}
+function noAddressInput(){
+    var bgcolor = document.getElementById("adddress");
+    var hex = "#A68585";
+    bgcolor.style.border = hex;
+}
+function noGenderInput(){
+    var bgcolor = document.getElementById("gender");
+    var hex = "#A68585";
+    bgcolor.style.border = hex;
+}
+function noInterestsInput(){
+    var bgcolor = document.getElementById("interests");
+    var hex = "#A68585";
+    bgcolor.style.border = hex;
+}
+function hasNameInput(){
+    var bgcolor = document.getElementById("name");
+    var hex = "#84A59D";
+    bgcolor.style.border = hex;
+}
+function hasUserInput(){
+    var bgcolor = document.getElementById("user");
+    var hex = "#84A59D";
+    bgcolor.style.border = hex;
+}
+function hasEmailInput(){
+    var bgcolor = document.getElementById("email");
+    var hex = "#84A59D";
+    bgcolor.style.border = hex;
+}
+function hasPassInput(){
+    var bgcolor = document.getElementById("pass");
+    var hex = "#84A59D";
+    bgcolor.style.border = hex;
+}
+function hasAddressInput(){
+    var bgcolor = document.getElementById("address");
+    var hex = "#84A59D";
+    bgcolor.style.border = hex;
+}
+function hasGenderInput(){
+    var bgcolor = document.getElementById("gender");
+    var hex = "#84A59D";
+    bgcolor.style.border = hex;
+}
+function hasInterestsInput(){
+    var bgcolor = document.getElementById("interests");
+    var hex = "#84A59D";
+    bgcolor.style.border = hex;
+}
